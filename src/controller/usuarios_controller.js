a59ddb17820ba6f900c9e61b291a4df54fad32cf
josebernal321 <61957579+josebernal321@users.noreply.github.com>
2021-03-07 22:32:14 -0300

    user controller and routers

--- src/controller/usuarios_controller.js
+++ src/controller/usuarios_controller.js
@@ -1,7 +1,52 @@
-
-async function getUsers(req,res){
+import {
+    User
+} from '../database/database'
+async function getUsers(req, res) {
     const msg = req.params.palabra;
-    res.status(200).json({state:'ok',msg});
+    res.status(200).json({
+        state: 'ok',
+        msg
+    });
 }
 
-module.exports={getUsers}
+async function login(req, res) {
+    try {
+        const {
+            email,
+            password
+        } = req.body;
+        const user = await User.findOne({
+            where: {
+                email
+            },
+        });
+
+        if(!user){
+            return res.status(404).json({
+                status:'404',
+                message:'Usuario o contraseña incorrecta'
+            })
+        }
+
+        if(password==user.password){
+            return res.status(404).json({
+                status:'404',
+                message:'Usuario o contraseña incorrecta'
+            })
+        }
+
+        res.status(200).json({
+            status:'200',
+            message:'Sesion iniciada'
+        })
+    } catch (error) {
+        return res.status(500).json({
+            status:'500',
+            message:'error al iniciar sesion'
+        })
+    }
+}
+
+module.exports = {
+    getUsers,login
+}
